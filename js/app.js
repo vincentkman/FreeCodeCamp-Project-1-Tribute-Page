5c5e0559a7d2ca804e0b5f2b7104856cd9bd87ed
Vincent Man <vincentman2702@hotmail.com>
2019-04-23 11:16:11 +0100

    Update project

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// ======================================
+//  Water Ripple Effect
+// ======================================
+
+$('body, footer').ripples({
+	resolution: 512,
+	dropRadius: 20,
+	perturbance: 0.04,
+});
